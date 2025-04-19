939f50cc08a79a67e4e778f0dd18b4bba2eab5c7
sanjay <bariyasanjay5430@gmail.com>
2025-04-19 22:34:31 +0530

    design issue fix

--- resources/views/items/cart.blade.php
+++ resources/views/items/cart.blade.php
@@ -3,5 +3,26 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 @section('page-content')
+<style>
+    .table td {
+    padding: 10px !important;
+    border: 0px;
+    border-bottom: 1px solid #DCDFE8;
+    color: #110A57;
+}
+.small-text-input {
+    width: 100px;
+    height: 25px;
+    font-size: 12px;
+    padding: 4px;
+}
+.form-control {
+    height: 30px ! important;
+  
+}
+.mb-4, .my-4{
+    margin-bottom: 0px !important;
+}
+</style>
     <livewire:shoppingcart />
 @endsection
