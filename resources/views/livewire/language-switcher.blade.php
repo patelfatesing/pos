b5457449e8e5a8baca732bea911329019f1589da
sanjay <bariyasanjay5430@gmail.com>
2025-05-03 12:39:12 +0530

    file addedd

---
+++ resources/views/livewire/language-switcher.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <select wire:model="language" wire:change="changeLanguage" class="form-control">
+        <option value="en">English</option>
+        <option value="hi">हिन्दी</option>
+    </select>
+</div>
