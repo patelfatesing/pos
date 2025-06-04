1200948289d76507714c5855a8f5a4f9ce364eda
patelfatesing <patel.fatesing36@gmail.com>
2025-06-04 23:11:47 +0530

    Resolved some issue

--- resources/views/notification/stock-request-form.blade.php
+++ resources/views/notification/stock-request-form.blade.php
@@ -7,7 +7,7 @@
     @csrf
     <div class="modal-header">
         <h5 class="modal-title" id="approveModalLabel">Stock Request Detail</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-bs-dismiss="modal" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
@@ -111,7 +111,7 @@
     </div>
 
     <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-dismiss="modal">Close</button>
         {{-- <button type="submit" class="btn btn-primary">Save changes</button> --}}
     </div>
 </form>
