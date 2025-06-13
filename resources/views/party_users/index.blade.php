abae7addeb3a2b48c5bbf415d06c9a693a0ae272
patelfatesing <patel.fatesing36@gmail.com>
2025-06-13 23:51:26 +0530

    some changes and issue fix

--- resources/views/party_users/index.blade.php
+++ resources/views/party_users/index.blade.php
@@ -1,44 +1,48 @@
 @extends('layouts.backend.layouts')
 
 @section('page-content')
+    {{-- CSRF & Scripts --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Wrapper Start -->
     <div class="wrapper">
         <div class="content-page">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <div class="d-flex flex-wrap align-items-center justify-content-between mb-4">
-                            <div>
-                                <h4 class="mb-3">Party Customer List</h4>
-                            </div>
-                            <a href="{{ route('party-users.create') }}" class="btn btn-primary add-list">
-                                <i class="las la-plus mr-3"></i>Add New Party Customer
-                            </a>
+                    <div class="col-lg-12 d-flex flex-wrap align-items-center justify-content-between mb-4">
+                        <div>
+                            <h4 class="mb-3">Party Customer List</h4>
                         </div>
+                        <a href="{{ route('party-users.create') }}" class="btn btn-primary add-list">
+                            <i class="las la-plus mr-3"></i>Add New Party Customer
+                        </a>
                     </div>
+
                     <div class="col-lg-12">
                         <div class="table-responsive rounded mb-3">
-                            <table class="table data-tables table-striped" id="party_users_table">
+                            <table class="table table-striped table-bordered nowrap" id="party_users_table" style="width:100%;">
                                 <thead class="bg-white text-uppercase">
-                                    <tr class="ligth ligth-data">
+                                    <tr class="light light-data">
                                         <th>Customer Name</th>
                                         <th>Email</th>
                                         <th>Phone</th>
                                         <th>Credit</th>
                                         <th>Status</th>
-                                        <th data-type="date" data-format="YYYY/DD/MM">Created Date</th>
-                                        <th data-type="date" data-format="YYYY/DD/MM">Updated Date</th>
+                                        <th>Created Date</th>
+                                        <th>Updated Date</th>
                                         <th>Actions</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                </tbody>
+                                <tbody></tbody>
                             </table>
                         </div>
                     </div>
@@ -46,83 +50,57 @@
             </div>
         </div>
     </div>
-    <!-- Wrapper End -->
 
+    <!-- Modal -->
     <div class="modal fade bd-example-modal-lg" id="custPriceChangeModal" tabindex="-1" role="dialog"
         aria-labelledby="custPriceChangeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content" id="custModalContent">
-            </div>
+            <div class="modal-content" id="custModalContent"></div>
         </div>
     </div>
 
     <script>
         $(document).ready(function() {
+            // Setup CSRF token
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
 
+            // Destroy any existing instance
             $('#party_users_table').DataTable().clear().destroy();
 
+            // Initialize DataTable
             $('#party_users_table').DataTable({
-                pageLength: 10,
-                responsive: true,
                 processing: true,
                 serverSide: true,
+                responsive: true,
                 ajax: {
                     url: '{{ url('party-users/get-data') }}',
                     type: 'POST',
                 },
-                columns: [{
-                        data: 'first_name'
-                    },
-                    {
-                        data: 'email'
-                    },
-                    {
-                        data: 'phone'
-                    },
-                    {
-                        data: 'credit_points'
-                    },
-                    {
-                        data: 'status'
-                    },
-                    {
-                        data: 'created_at'
-                    },
-                    {
-                        data: 'updated_at'
-                    },
-                    //  {
-                    //     data: 'is_delete'
-                    // },
-                    {
-                        data: 'action',
-                        orderable: false,
-                        searchable: false
-                    }
+                columns: [
+                    { data: 'first_name' },
+                    { data: 'email' },
+                    { data: 'phone' },
+                    { data: 'credit_points' },
+                    { data: 'status' },
+                    { data: 'created_at' },
+                    { data: 'updated_at' },
+                    { data: 'action', orderable: false, searchable: false }
                 ],
-                aoColumnDefs: [{
-                    bSortable: false,
-                    aTargets: [4, 5] // make "action" column unsortable
-                }],
-                order: [
-                    [6, 'desc']
-                ], // 🟢 Sort by created_at DESC by default
-                dom: "Bfrtip",
-                buttons: ['pageLength'],
+                order: [[6, 'desc']], // Sort by updated_at descending
                 lengthMenu: [
-                    [10, 25, 50],
-                    ['10 rows', '25 rows', '50 rows']
-                ]
+                    [10, 25, 50, 100, -1],
+                    [10, 25, 50, 100, "All"]
+                ],
+                pageLength: 10,
+                dom: '<"row mb-3"<"col-sm-6"l><"col-sm-6"f>>rt<"row mt-3"<"col-sm-6"i><"col-sm-6"p>>'
             });
-
-
         });
 
+        // Delete party user
         function delete_party_user(id) {
             Swal.fire({
                 title: "Are you sure?",
@@ -147,22 +125,22 @@
             });
         }
 
+        // Open price change modal
         function party_cust_price_change(id) {
-
             $.ajax({
                 url: '/cust-product-price-change/form/' + id,
                 type: 'GET',
                 success: function(response) {
-
                     $('#custModalContent').html(response);
                     $('#custPriceChangeModal').modal('show');
                 },
                 error: function() {
-                    alert('Failed to load form.');
+                    Swal.fire("Error", "Failed to load form.", "error");
                 }
             });
         }
 
+        // Status change
         function statusChange(id, newStatus) {
             Swal.fire({
                 title: "Are you sure?",
@@ -174,18 +152,14 @@
                 if (result.isConfirmed) {
                     $.ajax({
                         type: "POST",
-                        url: "{{ url('party-users/status-change') }}", // Update this to your route
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                        },
+                        url: "{{ url('party-users/status-change') }}",
                         data: {
                             id: id,
                             status: newStatus
                         },
                         success: function(response) {
                             Swal.fire("Success!", "Status has been changed.", "success").then(() => {
-                                $('#party_users_table').DataTable().ajax.reload(null,
-                                    false); // ✅ Only reload DataTable
+                                $('#party_users_table').DataTable().ajax.reload(null, false);
                             });
                         },
                         error: function(xhr) {
