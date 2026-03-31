c9f71e33b3725d982c6d9a88872bc378f214f4aa
patelfatesing <patel.fatesing36@gmail.com>
2026-03-31 18:02:58 +0000

    Resolved issue in staging2

--- app/Http/Controllers/PartyUserController.php
+++ app/Http/Controllers/PartyUserController.php
@@ -442,60 +442,29 @@ class PartyUserController extends Controller
         $start = $request->input('start', 0);
         $length = $request->input('length', 10);
         $searchValue = $request->input('search.value', '');
-        $orderColumnIndex = $request->input('order.0.column', 0);
-        $orderDirection = $request->input('order.0.dir', 'desc');
 
-        $columns = [
-            'invoice_id',
-            'invoice_number',
-            'invoice_date',
-            'invoice_total',
-            'commission_amount',
-            'commission_user_id',
-            'commission_user_name',
-            'commission_type',
-            'commission_value',
-            'applies_to',
-            'start_date',
-            'end_date'
-        ];
-
-        $orderColumn = $columns[$orderColumnIndex] ?? 'invoice_date';
-        if (!in_array($orderDirection, ['asc', 'desc'])) {
-            $orderDirection = 'desc';
-        }
-
-        // Base query
         $query = DB::table('invoices as i')
             ->select(
                 'i.id as invoice_id',
                 'i.invoice_number',
                 'i.created_at as invoice_date',
                 'i.total as invoice_total',
-                'i.creditpay as commission_amount',
-                'cu.id as party_user_id',
-                'cu.first_name as commission_user_name',
-                'cu.credit_points',
-                'ch.total_purchase_items',
+
                 'ch.credit_amount',
                 'ch.status',
                 'ch.id as commission_id',
+                'ch.created_at',
                 'pi.image_path',
-                'pi.id as party_user_image_id',
                 'pi.transaction_id',
-                'pi.type',
+
+                'cu.id as party_user_id',
+                'cu.first_name as commission_user_name'
             )
             ->leftJoin('credit_histories as ch', 'i.id', '=', 'ch.invoice_id')
             ->leftJoin('party_users as cu', 'ch.party_user_id', '=', 'cu.id')
             ->leftJoin('party_images as pi', 'i.id', '=', 'pi.transaction_id')
             ->whereNotNull('i.party_user_id');
 
-        // Total record count before filters
-        $recordsTotal = DB::table('invoices')
-            ->whereNotNull('party_user_id')
-            ->count();
-
-        // Apply search filter
         if (!empty($searchValue)) {
             $query->where(function ($q) use ($searchValue) {
                 $q->where('i.invoice_number', 'like', "%$searchValue%")
@@ -507,12 +476,14 @@ class PartyUserController extends Controller
             $query->where('cu.id', $request->customer_id);
         }
 
-        // Count after filters
-        $recordsFiltered = $query->count();
+        $recordsTotal = DB::table('invoices')
+            ->whereNotNull('party_user_id')
+            ->count();
 
-        // Get data with order and pagination
+        $recordsFiltered = (clone $query)->count();
+
         $data = $query
-            ->orderBy($orderColumn, $orderDirection)
+            ->orderBy('i.created_at', 'desc')
             ->offset($start)
             ->limit($length)
             ->get();
@@ -524,6 +495,7 @@ class PartyUserController extends Controller
             'data' => $data,
         ]);
     }
+
     public function getCreditHistory(Request $request)
     {
         $draw = $request->input('draw', 1);
@@ -569,7 +541,7 @@ class PartyUserController extends Controller
                 'ch.credit_amount',
                 'ch.debit_amount',
                 'ch.status',
-                'ch.created_at',
+                'ch.created_at as trasaction_date',
                 'ch.id as commission_id',
                 'pi.image_path',
                 'pi.id as party_user_image_id',
